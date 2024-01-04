109d4a7d63c1ae36b9b4f6b8bc50c03d19954cbf
Luka Dimjašević <luka@dimjasevic.net>
2024-01-04 22:12:32 +0100

    [IMP] server: Improvements

--- server/api/v1/helpers/error/index.ts
+++ server/api/v1/helpers/error/index.ts
@@ -4,6 +4,7 @@ import { HttpErrorForbidden } from "./http-error-forbidden";
 import { HttpErrorInternalServerError } from "./http-error-internal-server-error";
 import { HttpErrorNotFound } from "./http-error-not-found";
 import { HttpErrorUnauthorized } from "./http-error-unauthorized";
+import { HttpError } from "./http-error";
 
 export {
     HttpErrorBadRequest,
@@ -12,4 +13,5 @@ export {
     HttpErrorInternalServerError,
     HttpErrorNotFound,
     HttpErrorUnauthorized,
+    HttpError,
 }
