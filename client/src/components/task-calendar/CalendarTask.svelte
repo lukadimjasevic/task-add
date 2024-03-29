35c0368702ee86d1213c598e4fe3efc17f77f61f
Luka Dimjašević <luka@dimjasevic.net>
2024-03-29 15:50:45 +0100

    [IMP] client: Added task edit action to the calendar page

--- client/src/components/task-calendar/CalendarTask.svelte
+++ client/src/components/task-calendar/CalendarTask.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
     import type { Task } from "taskadd/task";
+    import TaskActionEdit from "@components/task-upcoming/TaskActionEdit.svelte";
 
     export let task: Task;
+
+    let showEditModal: boolean = false;
 </script>
 
-<button type="button" class="btn btn-sm btn-quaternary m-1">
+<button type="button" class="btn btn-sm btn-quaternary m-1" on:click={() => showEditModal = true}>
     {task.name}
 </button>
+
+<TaskActionEdit bind:show={showEditModal} {task} />
