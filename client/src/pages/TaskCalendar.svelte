93f99b93175a689ed1515650d8a3402a6279050f
Luka Dimjašević <luka@dimjasevic.net>
2024-03-29 15:36:25 +0100

    [IMP] client: Added task add action to the calendar page

--- client/src/pages/TaskCalendar.svelte
+++ client/src/pages/TaskCalendar.svelte
@@ -5,9 +5,17 @@
     import CalendarDay from "@components/task-calendar/day/CalendarDay.svelte";
     import CalendarWeek from "@components/task-calendar/week/CalendarWeek.svelte";
     import CalendarMonth from "@components/task-calendar/month/CalendarMonth.svelte";
+    import TaskActionAdd from "@components/task-upcoming/TaskActionAdd.svelte";
 </script>
 
-<h2>{$calendar.label}</h2>
+<div class="d-flex justify-content-between align-items-center">
+    <div>
+        <h2>{$calendar.label}</h2>
+    </div>
+    <div>
+        <TaskActionAdd/>
+    </div>
+</div>
 <div class="d-flex justify-content-between align-items-center">
     <CalendarSelector/>
     <CalendarPreviousNext/>
