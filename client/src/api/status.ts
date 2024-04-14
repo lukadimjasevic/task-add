3a8105524df2b95cc3644fbff3cbe0274ddbfe7d
Luka Dimjašević <luka@dimjasevic.net>
2024-04-14 12:57:24 +0200

    [IMP] client: Added response notifications for task status routes

--- client/src/api/status.ts
+++ client/src/api/status.ts
@@ -1,6 +1,7 @@
 import { request } from "./request";
+import type { Response } from "taskadd/response";
 
-const getAll = async() => {
+const getAll = async(): Promise<Response> => {
     return await request("/task-status", {
         method: "GET",
     });
--- client/src/components/task-upcoming/TaskStatuses.svelte
+++ client/src/components/task-upcoming/TaskStatuses.svelte
@@ -2,6 +2,7 @@
     import { tasks } from "@stores/task";
     import { taskStatuses } from "@stores/task-status";
     import { api } from "@api";
+    import { helpers } from "@helpers";
     import { DropdownItem } from "@components/common/dropdowns";
     import type { Task, TaskUpdateDTO } from "taskadd/task";
     import type { TaskStatus } from "taskadd/task-status";
@@ -16,17 +17,12 @@
 
     const handleStatusUpdate = (status: TaskStatus) => {
         tasksFiltered.forEach(async(task: Task) => {
-            const dto: TaskUpdateDTO = {
-                id: task.id,
-                status: status.name,
-            }
+            const dto: TaskUpdateDTO = { id: task.id, status: status.name };
             const response = await api.task.update(dto);
-            if (response.statusCode === 200) {
+            helpers.response.handleResponse(response, "Task status update", async() => {
                 const fetchedTasks = await api.task.getAll();
-                if (fetchedTasks.statusCode === 200) {
-                    tasks.setValues(fetchedTasks.data);
-                }
-            }
+                tasks.setValues(fetchedTasks.data);
+            });
         });
     }
 </script>
