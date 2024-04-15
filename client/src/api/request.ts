a05d89d23edd458a3a29cc6881d54210d0f079b8
Luka Dimjašević <luka@dimjasevic.net>
2024-04-15 20:14:52 +0200

    [FIX] client: Fixes

--- client/src/api/request.ts
+++ client/src/api/request.ts
@@ -1,9 +1,7 @@
 import type { Response } from "taskadd/response";
 
-const server = "http://localhost:8080/api/v1";
-
 export const request = async(url: string, options: RequestInit): Promise<Response> => {
-    const response = await fetch(server + url, {
+    const response = await fetch(import.meta.env.VITE_API_URL + url, {
         headers: {
             "Content-Type": "application/json",
         },
